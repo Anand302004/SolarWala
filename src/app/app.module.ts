036096e457089c71b7869bef101dd2f931054c0e
Vishalchavan4640 <vc9291038@gmail.com>
2025-01-06 10:25:00 +0530

    i added ACDB-DCDB images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 import { SolarcleaningComponent } from './solarcleaning/solarcleaning.component';
+import { ACDBDCDBComponent } from './acdb-dcdb/acdb-dcdb.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { SolarcleaningComponent } from './solarcleaning/solarcleaning.component'
     GalleryComponent,
     AboutComponent,
     FormComponent,
-    SolarcleaningComponent
+    SolarcleaningComponent,
+    ACDBDCDBComponent
   ],
   imports: [
     BrowserModule,
